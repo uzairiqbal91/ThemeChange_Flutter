81a16839f8691e0bd8a9437eb86c1e43fcd3f3d8
uzairiqbal91 <uzairiqbal91@gmail.com>
2020-03-14 23:01:01 +0500

    initial commit

--- lib/constants/Globals.dart
+++ lib/constants/Globals.dart
@@ -6,7 +6,7 @@ class Globals{
   static  String Change_Password = "Change Password ";
   static  String Schedule_Settings = "Schedule Settings ";
   static  String Privacy_Policy = "Privacy Policy ";
-  static  String About_TFI = "About TFI ";
+  static  String About_TFI = "About ";
   static  String Help = "Help ";
   static  String Logout = "Logout ";
 }
